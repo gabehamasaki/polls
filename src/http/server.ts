fa0d7b5d98b3e9feac4c12f0f9c6ec3068f7e03e
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-02-09 09:52:45 -0300

    Finalizando projeto

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,21 +3,25 @@ import cookie from '@fastify/cookie'
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
 import { voteOnPoll } from './routes/vote-on-poll';
+import pollResults from './ws/poll-results';
 import websocket from '@fastify/websocket';
 
 const app = fastify()
-
-app.register(websocket);
 
 app.register(cookie, {
   secret: '$bacaf5fc23a34f389e9d00c83a24bb5$',
   hook: 'onRequest',
 })
 
+app.register(websocket);
+
 // Polls Routes
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+
+// Websocket Routes
+app.register(pollResults)
 
 app.listen({
   port: 3333,
